904743bdbf7cbaf2d410ca03f5bfb43603470b43
SantiagoCastro07 <santiagocastro3110@gmail.com>
2025-11-09 21:16:22 -0500

    Update SistemaUsuarios.java

--- ejercicio1_sistemausuarios/SistemaUsuarios.java
+++ ejercicio1_sistemausuarios/SistemaUsuarios.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+package ejercicio1_sistemausuarios;
 
-package com.mycompany.sistemacalificaciones;
-
-/**
- *
- * @author ashly
- */
 public class SistemaUsuarios {
 
     public static void main(String[] args) {
