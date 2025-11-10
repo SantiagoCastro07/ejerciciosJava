c2b5ee9e0803354a4b7f2c513f916c1fbf09e2c8
SantiagoCastro07 <santiagocastro3110@gmail.com>
2025-11-09 21:17:07 -0500

    Refactor User.java by removing comments and changing package
    Removed license comments and updated package name.

--- ejercicio1_sistemausuarios/User.java
+++ ejercicio1_sistemausuarios/User.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.sistemacalificaciones;
+package ejercicio1_sistemausuarios;
 
-/**
- *
- * @author ashly
- */
 public class User {
     private String name;
     private int id;
@@ -18,10 +10,6 @@ public class User {
         this.id = id;
         this.password = password;
     }
-    
-    //constructor vacio crea y inicializa el constructor vacio
-    //  public User(){
-    //  }
     
     public String getNombre(){
         return name;
